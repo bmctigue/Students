eee8180caddd5d3415b9d86845d6342e41de80e2
Bruce McTigue <bmctigue@tiguer.com>
2019-08-28 06:35:44 -0600

    small change

--- Students/Scenes/Students/View/StudentsTableViewController.swift
+++ Students/Scenes/Students/View/StudentsTableViewController.swift
@@ -32,6 +32,10 @@ final class StudentsTableViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.rowHeight = UITableView.automaticDimension
@@ -70,9 +74,5 @@ final class StudentsTableViewController: UIViewController {
         self.tableView.reloadData {
             self.tableView.scroll(to: .top, animated: true)
         }
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        return nil
     }
 }
